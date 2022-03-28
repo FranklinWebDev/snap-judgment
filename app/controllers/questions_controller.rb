b85662bb4563b28fdd8d2022d7d1524b53056cbd
Richard Ledger <richard.ledger@lanebryant.com>
2022-03-28 15:30:36 -0500

    minor copy tweaks

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,18 +10,18 @@ class QuestionsController < ApplicationController
 	end
 	def create
 		Question.create(question_params)
-		redirect_to questions_path
+		redirect_to questions_path,  alert: "Question has added."
 	end
 	def edit
 		@question = Question.find(params[:id])
 	end
 	def update
 		Question.update(params[:id],question_params)
-		redirect_to questions_path
+		redirect_to questions_path,  alert: "Question has been updated."
 	end
 	def destroy
 		Question.destroy(params[:id])
-		redirect_to questions_path, alert: "question has been deleted."
+		redirect_to questions_path, alert: "Question has been deleted."
 	end
 	def question_params
 		params.require(:question).permit(:question_image, :situation, :description, :hint, :option1, :option2, :option3, :option4, :answer)
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,3 @@
 class Question < ApplicationRecord
 	has_one_attached :question_image
-	require 'csv'
-	def self.add_from_csv(params)
-		CSV.foreach(params[:questionsCSV], headers: true) do |row|
-			Question.create(row.to_h)
-		end
-	end
 end
