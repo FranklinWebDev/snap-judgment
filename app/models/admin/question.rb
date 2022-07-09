aaaf6b97e542422e71e2544086d0e6d1b8b801df
Dani Hooven <daniellehooven@gmail.com>
2022-07-09 19:41:41 -0400

    Added associations

--- app/models/admin/question.rb
+++ app/models/admin/question.rb
@@ -1,4 +1,7 @@
 class Admin::Question < ApplicationRecord
+	belongs_to :quiz
 	has_one_attached :question_image
 	has_many :answers, :dependent => :destroy
+	accepts_nested_attributes_for :answers
+
 end
