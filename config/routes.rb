5fd7ac511ad00a31a47cddc6e0693109e4539251
Wessam Montasser <59712964+wmontasser@users.noreply.github.com>
2022-07-13 03:28:21 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   get 'login', to: 'sessions#new'
 
-  get 'admin', to: 'admin/users#index'
+  get 'admin', to: 'admin#users#index'
 
   get 'forgot_password', to: 'forgot_password#index'
 
