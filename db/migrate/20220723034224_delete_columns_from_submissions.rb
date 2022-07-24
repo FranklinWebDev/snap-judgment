ec9ab5c94dac85136b32529dc16ed79f68dcd4d5
Dani Hooven <daniellehooven@gmail.com>
2022-07-24 13:43:50 -0400

    Add if_exists clause to delete submission columns migration

--- db/migrate/20220723034224_delete_columns_from_submissions.rb
+++ db/migrate/20220723034224_delete_columns_from_submissions.rb
@@ -1,17 +1,17 @@
 class DeleteColumnsFromSubmissions < ActiveRecord::Migration[7.0]
   def change
     
-    remove_foreign_key :submissions, :quizzes
-    remove_foreign_key :submissions, :answers
-    remove_foreign_key :submissions, :users 
+    remove_foreign_key :submissions, :quizzes, if_exists: true
+    remove_foreign_key :submissions, :answers, if_exists: true
+    remove_foreign_key :submissions, :users, if_exists: true
   
-    remove_reference :submissions, :quiz, foreign_key: false, index: false
-    remove_reference :submissions, :answer, foreign_key: false, index: false
-    remove_reference :submissions, :user, foreign_key: false, index: false
+    remove_reference :submissions, :quiz, foreign_key: false, index: false, if_exists: true
+    remove_reference :submissions, :answer, foreign_key: false, index: false, if_exists: true
+    remove_reference :submissions, :user, foreign_key: false, index: false, if_exists: true
     add_reference :submissions, :result, foreign_key: true, index: true
 
     add_column :submissions, :answer, :string
-    remove_column :submissions, :is_correct, :boolean
+    remove_column :submissions, :is_correct, :boolean, if_exists: true
   
   end
 end
