54ede2397b603c2596fc7232fc3b9d31d5123155
Dani Hooven <daniellehooven@gmail.com>
2022-07-24 13:46:41 -0400

    commented out lines related to user reference and foreign key

--- db/migrate/20220723034224_delete_columns_from_submissions.rb
+++ db/migrate/20220723034224_delete_columns_from_submissions.rb
@@ -3,11 +3,11 @@ class DeleteColumnsFromSubmissions < ActiveRecord::Migration[7.0]
     
     remove_foreign_key :submissions, :quizzes, if_exists: true
     remove_foreign_key :submissions, :answers, if_exists: true
-    remove_foreign_key :submissions, :users, if_exists: true
+    # remove_foreign_key :submissions, :users, if_exists: true
   
     remove_reference :submissions, :quiz, foreign_key: false, index: false, if_exists: true
     remove_reference :submissions, :answer, foreign_key: false, index: false, if_exists: true
-    remove_reference :submissions, :user, foreign_key: false, index: false, if_exists: true
+    # remove_reference :submissions, :user, foreign_key: false, index: false, if_exists: true
     add_reference :submissions, :result, foreign_key: true, index: true
 
     add_column :submissions, :answer, :string
