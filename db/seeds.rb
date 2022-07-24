675ec3244782d73ffa336804b9e2f9a3430bb2f6
Dani Hooven <daniellehooven@gmail.com>
2022-07-24 12:48:26 -0400

    destroy existing submission objects on seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Submission.destroy_all
+
 Quiz.destroy_all
 
 # Default Quiz
