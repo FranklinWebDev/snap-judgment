1ddf1566c1ca326ec93622489ded24994cf44472
Dani Hooven <daniellehooven@gmail.com>
2022-10-10 17:18:06 -0400

    reverting authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   require 'will_paginate/array'
-  include Pundit::Authorization
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  # include Pundit::Authorization
+  # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   # protect_from_forgery with: :null_session, if: ->(controller) { controller.request.format == "application/json" }
   # before_action :configure_permitted_parameters, if: :devise_controller?
